e583d4d354de180a06c5b389d49d9eb6e2c2ed7b
fahimshafeek <fahimshafeek@gmail.com>
2026-09-12 19:03:15 +0530

    Clean up debugging files

--- parse_workflows.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('/home/fahim/projects/useless/workflows.json', 'r') as f:
-    data = json.load(f)
-for wf in data:
-    for node in wf.get('nodes', []):
-        if 'if' in node.get('type', '').lower():
-            print(json.dumps(node, indent=2))
-            break
